7f490ca00c27a6c35583989d7576b78dc9d01c57
Dicksen Tan <tandicksen@gmail.com>
2024-09-14 08:57:32 +0700

    dotenv dependencies

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,15 @@
+//called so we can use whatever in .env file by using process.env
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
-app.listen(3000)
+app.get('/', (req, res) =>{
+    res
+})
+
+// using port based on what in .env project to prevent info leak
+app.listen(process.env.PORT,() =>{
+    console.log(`Listening to port ${process.env.PORT}`);
+    
+})
